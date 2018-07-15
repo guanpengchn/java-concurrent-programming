9faefe8354284257a7e0cd596cf7678a042a4cf5
guanpengchn <875499765@qq.com>
2018-07-15 14:36:01 +0800

    feat(chapter2) add more

---
+++ src/chapter2/AccountingVol.java
@@ -0,0 +1,48 @@
+package chapter2;
+
+public class AccountingVol implements Runnable {
+    static AccountingVol instance = new AccountingVol();
+    static volatile int i = 0;
+
+//    public static void increase() {
+//        i++;
+//    }
+//
+//    @Override
+//    public void run() {
+//        for (int j = 0; j < 10000000; j++) {
+//            increase();
+//        }
+//    }
+
+//    @Override
+//    public void run() {
+//        for (int j = 0; j < 10000000; j++) {
+//            synchronized (instance){
+//                i++;
+//            }
+//        }
+//    }
+
+    public synchronized static void increase() {
+        i++;
+    }
+
+    @Override
+    public void run() {
+        for (int j = 0; j < 10000000; j++) {
+            increase();
+        }
+    }
+
+    // synchronized锁的是对象，不是函数
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new Thread(instance);
+        Thread t2 = new Thread(instance);
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+        System.out.println(i);
+    }
+}
